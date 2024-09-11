9429f898a525530e70c606572dc26205620d28cc
Paulsubh <subhajitp4321@gmail.com>
2024-09-11 09:43:18 +0530

    11/9/24

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4003;
+const PORT = process.env.PORT || 5005;
 const URI = process.env.MongoDBURI;
 
 //connect to mongoDB
